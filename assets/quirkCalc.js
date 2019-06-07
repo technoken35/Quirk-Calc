98d804d7360c53c8c95ce328f8e85aa3f4015579
Kendall Hayes <hayeskendall21@gmail.com>
2019-06-06 23:30:55 -0700

    Added method to let calculator know there here has been two operand press. Calculator now executes on the second operand press.

--- assets/quirkCalc.js
+++ assets/quirkCalc.js
@@ -2,23 +2,23 @@ var displayOne, operandDOM, clearDOM, btnClick, equalDOM, divisionDOM, additionD
 
 var oneDOM, twoDOM, threeDOM, fourDOM, fiveDOM, sixDOM, sevenDOM, eightDOM, nineDOM, currentINPUT, ongoingInput, deleteDOM, decimalDOM;
 
-var operandClicked, operand, testTotal,testing123,testing1234,waitingForSecondOperandPress,operand1,testArray;
+var operandClicked, operand, testTotal, testing123, testing1234, waitingForSecondOperandPress, operand1, testArray,calculatorRunning;
 
-operand1=null;
+operand1 = null;
 
-testArray=[null,null];
+testArray = [null, null];
 //test array to store variables instead of variable because it is more dynamic
 
 
-
 /* 
-! TODO: Selecting individual value buttons may not be neccessay, with array 
 ? need a way to let program know there has been two clicks
 if arrays are both equal
 test commit
 ? Questions
-*/
 
+! FIX ME: Stop the first operand press from triggering the second
+
+*/
 
 
 displayOne = "";
@@ -39,7 +39,6 @@ equalDOM = document.getElementById("equal");
 
 // assigning values to buttons is better
 additionDOM = document.getElementById("addition");
-
 
 var display = [null, null];
 
@@ -70,7 +69,6 @@ decimalDOM.onclick = function () {
     // 5. return variable to ship back to calculator function 
 }
 
-
 equalDOM.addEventListener("click", function () {
     // if equals is pressed
 
@@ -86,14 +84,12 @@ equalDOM.addEventListener("click", function () {
 
     // add new value to expression string
 
-
 });
 
 clearDOM.addEventListener("click", function () {
     screenDOM.textContent = "";
     // clear all other input variables
 });
-
 
 for (i = 0; i < btnClick.length; i++) {
     btnClick[i].addEventListener("click", function () {
@@ -103,58 +99,82 @@ for (i = 0; i < btnClick.length; i++) {
         //  recieves NEW value on EVERY click
 
         ongoingInput += currentINPUT;
+        // keeps ongoing input in variable
 
-        display.splice(0, 0, currentINPUT);
-        // * * Splices the array to update the first value of so "null" does not appear in display screen
+        screenDOM.textContent += currentINPUT;
+        // screenDOM is constantly appended. Adding the new button pressed to it's total
 
+        if (operandClicked && testArray[0] !== null) {
+            // instructions after FIRST operand press
+        
+            testArray[1] = Number(screenDOM.textContent);
+            // start collecting screenDOM value in second index
+            console.log("you have reached the second level click in the button listener");
+            operand1=operand;
+            // store first operand pressed for execution
 
-        screenDOM.textContent += currentINPUT
+        } else {
+            // if operandClicked is false and the first array[0] is null
+            // add screenDOM value to first array[0]
+            testArray[0] = Number(screenDOM.textContent);
 
-    
-       
+        }
 
     });
-
 
 }
 
 for (i = 0; i < operandDOM.length; i++) {
+    // listen to all operand buttons
     operandDOM[i].addEventListener("click", function () {
-       
-
-        if(testArray[0]===null) {
-            //collect input in first operand
+            
             firstOperandPress();
-            //first operand clicked is true
-        }
+        // if operand clicked is true & all test arrays have values
+        if (operandClicked && testArray[0] !== null && testArray[1] !== null) {
+            // call calculator
+            console.log("This is the calculator call");
+            var testGrandTotal = calculator[operand1](testArray[0],testArray[1]);
+            console.log("final calculator call"+ " " + "this is the grand total" + " " + testGrandTotal);
 
-        secondOperandPress();
+            // array[0] now holds grand total
+            testArray[0] = testGrandTotal;
+            console.log("array[0] after calculation" + testArray[0]);
+            // clear operand
+            operand1="";
+            // clear second array value
+            testArray[1]= null;
+
+        // we cycle operand variable in calculator object to call the corresponding function
+
+        // state variable to clarify that calculator is running
+            calculatorRunning=true;
+        } else if(calculatorRunning && testArray[0] !==null) {
+                testArray[1] = Number(screenDOM.textContent);
+                console.log("after second calculator is running" + " " + testArray[1]);
+        }
 
 
         var operandValue = Number(this.value);
         // collects operand button "value"
 
-
         // depending on value choose what the operand will be
         if (operandValue === 0) {
             operand = "addition";
-            console.log("hi");
+            console.log("hi" + " " + operand);
+            return operand;
 
         } else if (operandValue === 1) {
             operand = "subtraction";
+            return operand;
         } else if (operandValue === 2) {
             operand = "multiplication";
+            return operand;
         } else if (operandValue === 3) {
             operand = "division"
             console.log("ho");
+            return operand;
         }
-        
-        operand1=operand;
-
         // if else statements cycle operand based on button value
-
-        var testGrandTotal = calculator[operand]();
-        // we cycle operand variable in calculator object to call the corresponding function
 
 
         console.log(operand);
@@ -165,23 +185,30 @@ for (i = 0; i < operandDOM.length; i++) {
 }
 
 calculator = {
-    addition() {
-        testTotal = 5 + 5;
+    // current function is called depending on the value of the operand button pressed
+    addition(value1,value2) {
+        testTotal= value1 + value2;
+        console.log(testTotal + " " + " in calculator");
+        return testTotal;
+        // this works
+    },
+
+    subtraction(value1, value2) {
+        //takes array[0] & array[1] as arguements
+        testTotal = value1 - value2;
+        console.log(testTotal + " " + " in calculator");
         return testTotal;
     },
 
-    subtraction() {
-        testTotal = 5 - 5;
+    division(value1,value2) {
+        testTotal = value1 / value2;
+        console.log(testTotal + " " + " in calculator");
         return testTotal;
     },
 
-    division() {
-        testTotal = 10 / 5;
-        return testTotal;
-    },
-
-    multiplication() {
-        testTotal = 5 * 5;
+    multiplication(value1,value2) {
+        testTotal = value1 * value2;
+        console.log(testTotal + " " + " in calculator");
         return testTotal;
     }
 
@@ -190,38 +217,21 @@ calculator = {
 
 function firstOperandPress() {
 
+    console.log("here is the" + " " + operand + " " + "you requested" + " " + "it will probaly be undefined");
 
-    alert("my name is freddy");
-
-    testing123 = Number(screenDOM.textContent);
-
-    testArray[0]=Number(screenDOM.textContent);
-    console.log(testArray[0] + " " + "test array");
+    console.log("this is the first operand press");
 
     // operand clicked true
     operandClicked = true;
     // clear dom
     screenDOM.textContent = "";
     currentINPUT = "";
-    //cycle to array [1]
+    //waitingForSecondOperandPress=true;
 
-    waitingForSecondOperandPress=true;
-    
-}
-
-function secondOperandPress () {
-    if (waitingForSecondOperandPress ===true && testArray[0] !==null) {
-        console.log("youve reached the second operand click");
-        
-        testing1234 = Number(currentINPUT);
-        console.log(testing1234);
-
-        // may need to add function to button listener, so that the first operand press does not trigger the rest
-        
-
-    }
-
-    
 }
 
 
+function test () {
+    alert("test");
+    console.log("test");
+}
